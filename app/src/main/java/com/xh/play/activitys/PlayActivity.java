bc876a78f0266b0622ba692b6aae90bcf3e9ca5c
liuhuiliang <liuhuiliang@bianfeng.com>
2021-07-26 19:36:49 +0800

    添加隐藏状态栏和导航栏

--- app/src/main/java/com/xh/play/activitys/PlayActivity.java
+++ app/src/main/java/com/xh/play/activitys/PlayActivity.java
@@ -2,6 +2,7 @@ package com.xh.play.activitys;
 
 import android.animation.Animator;
 import android.animation.ObjectAnimator;
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
@@ -78,6 +79,7 @@ public class PlayActivity extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED, WindowManager
                 .LayoutParams.FLAG_HARDWARE_ACCELERATED);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
         setContentView(R.layout.activity_play);
         ButterKnife.bind(this);
         stateLLEmbllish = new ViewEmbellish(stateLL);
@@ -340,10 +342,21 @@ public class PlayActivity extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-        if (Build.VERSION.SDK_INT >= 21)
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+//        decorView.setSystemUiVisibility(
+//                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//                         |View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+//                         |View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+////                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+//                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+//                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+//    );
+        if (Build.VERSION.SDK_INT >= 21) {
             getWindow().setStatusBarColor(Color.TRANSPARENT);
+//            getWindow().setNavigationBarColor(Color.TRANSPARENT);
+        }
+
     }
 
     private class MyTap extends Tap {
