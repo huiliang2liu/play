7ceedd7f7fec76795e87301e769bb1b23e50c576
liuhuiliang <liuhuiliang@bianfeng.com>
2021-07-26 19:41:50 +0800

    a

--- app/src/main/java/com/xh/play/activitys/PlayActivity.java
+++ app/src/main/java/com/xh/play/activitys/PlayActivity.java
@@ -342,8 +342,7 @@ public class PlayActivity extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION|View.SYSTEM_UI_FLAG_FULLSCREEN);
 //        decorView.setSystemUiVisibility(
 //                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
 //                         |View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
@@ -354,7 +353,7 @@ public class PlayActivity extends Activity {
 //    );
         if (Build.VERSION.SDK_INT >= 21) {
             getWindow().setStatusBarColor(Color.TRANSPARENT);
-//            getWindow().setNavigationBarColor(Color.TRANSPARENT);
+            getWindow().setNavigationBarColor(Color.TRANSPARENT);
         }
 
     }
